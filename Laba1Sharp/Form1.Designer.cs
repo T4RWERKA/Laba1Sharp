5e1109f82b34959d5cc27cbf91ef6b3792c5a590
Denis Shafarenko <phisika001@mail.ru>
2023-03-19 17:12:52 +0300

    Добавьте файлы проекта.

---
+++ Laba1Sharp/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Laba1Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblDict = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblName1 = new System.Windows.Forms.Label();
+            this.tblOperators = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tblOperands = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.boxCode = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblOperators)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblOperands)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblVolume);
+            this.panel3.Controls.Add(this.lblLength);
+            this.panel3.Controls.Add(this.lblDict);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 1106);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1570, 444);
+            this.panel3.TabIndex = 2;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(288, 183);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(12, 25);
+            this.lblVolume.TabIndex = 5;
+            this.lblVolume.Text = "\r\n";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(288, 142);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(0, 25);
+            this.lblLength.TabIndex = 4;
+            // 
+            // lblDict
+            // 
+            this.lblDict.AutoSize = true;
+            this.lblDict.Location = new System.Drawing.Point(288, 102);
+            this.lblDict.Name = "lblDict";
+            this.lblDict.Size = new System.Drawing.Size(12, 25);
+            this.lblDict.TabIndex = 3;
+            this.lblDict.Text = "\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Объем программы:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Длина программы:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Словарь программы:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1570, 1097);
+            this.panel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1546, 555);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblName1);
+            this.panel2.Controls.Add(this.tblOperators);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(767, 549);
+            this.panel2.TabIndex = 0;
+            // 
+            // lblName1
+            // 
+            this.lblName1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblName1.AutoSize = true;
+            this.lblName1.Location = new System.Drawing.Point(164, 36);
+            this.lblName1.Name = "lblName1";
+            this.lblName1.Size = new System.Drawing.Size(220, 25);
+            this.lblName1.TabIndex = 1;
+            this.lblName1.Text = "Словарь операторов";
+            // 
+            // tblOperators
+            // 
+            this.tblOperators.AllowUserToAddRows = false;
+            this.tblOperators.AllowUserToDeleteRows = false;
+            this.tblOperators.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tblOperators.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblOperators.Location = new System.Drawing.Point(15, 64);
+            this.tblOperators.Name = "tblOperators";
+            this.tblOperators.ReadOnly = true;
+            this.tblOperators.RowHeadersWidth = 82;
+            this.tblOperators.RowTemplate.Height = 33;
+            this.tblOperators.Size = new System.Drawing.Size(502, 482);
+            this.tblOperators.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.tblOperands);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(776, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(767, 549);
+            this.panel4.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(401, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Словарь операндов";
+            // 
+            // tblOperands
+            // 
+            this.tblOperands.AllowUserToAddRows = false;
+            this.tblOperands.AllowUserToDeleteRows = false;
+            this.tblOperands.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tblOperands.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblOperands.Location = new System.Drawing.Point(262, 64);
+            this.tblOperands.Name = "tblOperands";
+            this.tblOperands.ReadOnly = true;
+            this.tblOperands.RowHeadersWidth = 82;
+            this.tblOperands.RowTemplate.Height = 33;
+            this.tblOperands.Size = new System.Drawing.Size(502, 482);
+            this.tblOperands.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.73858F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.26142F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel5, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(2564, 1559);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.panel3, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.08434F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.91566F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1576, 1553);
+            this.tableLayoutPanel3.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btnCalc);
+            this.panel5.Controls.Add(this.btnLoad);
+            this.panel5.Controls.Add(this.boxCode);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(1585, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(976, 1553);
+            this.panel5.TabIndex = 5;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(178, 766);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(131, 45);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(21, 766);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(131, 45);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Browse";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // boxCode
+            // 
+            this.boxCode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.boxCode.Location = new System.Drawing.Point(307, 18);
+            this.boxCode.Name = "boxCode";
+            this.boxCode.Size = new System.Drawing.Size(651, 742);
+            this.boxCode.TabIndex = 0;
+            this.boxCode.Text = "";
+            this.boxCode.TextChanged += new System.EventHandler(this.boxCode_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2564, 1559);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.MinimumSize = new System.Drawing.Size(2500, 1500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblOperators)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblOperands)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblDict;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblName1;
+        private System.Windows.Forms.DataGridView tblOperators;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView tblOperands;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.RichTextBox boxCode;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Laba1Sharp/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Laba1Sharp
+{
+    public partial class Form1 : Form
+    {
+        public List<string> code = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTable(tblOperands);
+            InitializeTable(tblOperators);
+        }
+
+        void InitializeTable(DataGridView table)
+        {
+            var numColumn = new DataGridViewTextBoxColumn();
+            var opColumn = new DataGridViewTextBoxColumn();
+            var countColumn = new DataGridViewTextBoxColumn();
+
+            numColumn.Name = "numColumn";
+            numColumn.HeaderText = "№";
+
+            if (table.Name == "tblOperators")
+                opColumn.HeaderText = "Оператор";
+            else
+                opColumn.HeaderText = "Операнд";
+
+            opColumn.Name = "opColumnColumn";
+
+
+            countColumn.Name = "countColumn";
+            countColumn.HeaderText = "Количество";
+
+            table.Columns.AddRange(numColumn, opColumn, countColumn);
+        }
+
+        public static void FillTable(DataGridView table, Dictionary<string, int> dictionary)
+        {
+            table.Rows.Clear();
+                     
+
+            int i = 1, sum = 0;
+            DataGridViewRow row;
+            DataGridViewTextBoxCell num, op, count;
+            foreach (var c in dictionary)
+            {
+                row = new DataGridViewRow();
+
+                num = new DataGridViewTextBoxCell();
+                op = new DataGridViewTextBoxCell();
+                count = new DataGridViewTextBoxCell();
+
+                num.Value = i++;
+                op.Value = c.Key;
+                count.Value = c.Value;
+                sum += c.Value;
+
+                row.Cells.AddRange(num, op, count);
+                table.Rows.Add(row);
+            }
+
+            row = new DataGridViewRow();
+            num = new DataGridViewTextBoxCell(); num.Value = "Sum";
+            op = new DataGridViewTextBoxCell(); op.Value = "";
+            count = new DataGridViewTextBoxCell(); count.Value = sum;
+            row.Cells.AddRange(num, op, count);
+            table.Rows.Add(row);
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var reader = new StreamReader(openFileDialog1.FileName);
+                code = new List<string>();
+                try
+                {
+                    while (!reader.EndOfStream)
+                        code.Add(reader.ReadLine());
+                }
+                finally
+                {
+                    reader.Close();
+                }
+                boxCode.Text = "";
+                foreach (var str in code)
+                    boxCode.Text += str + "\n";
+            }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            Halsted.FillDictionaries(code);
+            FillTable(tblOperands, Halsted.OperandDictionary);
+            FillTable(tblOperators, Halsted.OperatorDictionary);
+
+            int dict, length, volume;
+            Halsted.GetMetrics(out dict, out length, out volume);
+
+            lblDict.Text = dict.ToString();
+            lblLength.Text = length.ToString();
+            lblVolume.Text = volume.ToString();
+        }
+
+        private void boxCode_TextChanged(object sender, EventArgs e)
+        {
+            code = boxCode.Lines.ToList();
+        }
+    }
+}
